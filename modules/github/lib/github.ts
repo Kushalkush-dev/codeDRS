428ad48466aceae046fcc65db83bd1b0e1800f77
kushal <jkushal208@gmail.com>
2026-06-13 00:48:23 +0530

    update github.ts

--- modules/github/lib/github.ts
+++ modules/github/lib/github.ts
@@ -1,29 +1,29 @@
 import { auth } from "@/lib/auth"
 import prisma from "@/lib/db"
 import { headers } from "next/headers"
+import { Octokit } from "octokit"
 
 
 
-
-export const  getGithubAccessToken=async()=>{
-    const session=await auth.api.getSession({
-        headers:await headers()
+export const getGithubAccessToken = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
     })
 
-    if(!session){
+    if (!session) {
         throw new Error("Unauthorized Access")
     }
 
-    const account= await prisma.account.findFirst({
+    const account = await prisma.account.findFirst({
 
-        where:{
-            userId:session.user.id,
-            providerId:"github"
+        where: {
+            userId: session.user.id,
+            providerId: "github"
         }
     })
 
 
-    if(!account?.accessToken){
+    if (!account?.accessToken) {
         throw new Error("No Github Access Token Found")
     }
 
@@ -31,3 +31,39 @@ export const  getGithubAccessToken=async()=>{
 }
 
 
+export const fetchUserContribution = async (token: string, userName: string) => {
+    const octokit = new Octokit({ auth: token })
+
+    const query = `
+    query($Username:String!){
+         user(login:$username){
+             contributionCollection{
+                 contributionCalendar{
+                     weeks{
+                         contributionDays{
+                            contributionCount
+                            data
+                            color
+                            }
+                        }
+                     }
+                }
+            }
+     }
+    `
+
+    // interface contributionData{
+    // }
+    try {
+        const response: any = await octokit.graphql(query, {
+            username: userName
+        })
+
+        return response.user.contributionCollection.contributionCalender
+
+    } catch (error) {
+        console.error("Error fetching user contribution data:", error);
+        return null;
+    }
+
+}
