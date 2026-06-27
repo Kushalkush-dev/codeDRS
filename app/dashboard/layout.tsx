219de3189bcfb5562bee7f10fe73010dcb319517
kushal <jkushal208@gmail.com>
2026-06-28 01:12:10 +0530

    update the dashboard header with the app name:codeDRS

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,24 +6,29 @@ import { requireAuth } from '@/modules/auth/utils/auth-utils'
 import React from 'react'
 
 
-const DashboardLayout =async ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
     await requireAuth()
     return (
         <TooltipProvider>
-          <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                    <SidebarTrigger className="-ml-1" />
-                    <Separator orientation="vertical" className="mx-2 h-4" />
-                    <h1 className="text-xl font-semibold text-foreground">Dashboard</h1>
-                </header>
-                <main className="flex-1 overflow-auto p-4 md:p-6">
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator orientation="vertical" className="mx-2 h-4" />
+                        <span className="text-xl font-black tracking-normal">
+                            <span className="text-white">code</span>
+                            <span className="bg-gradient-to-r from-[#D84A4E] to-[#7042C7] bg-clip-text text-transparent ">
+                                DRS
+                            </span>
+                        </span>
+                    </header>
+                    <main className="flex-1 overflow-auto p-4 md:p-6">
 
-                    {children}
-                </main>
-            </SidebarInset>
-          </SidebarProvider>
+                        {children}
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
         </TooltipProvider>
     )
 }
